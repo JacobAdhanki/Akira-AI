2c2686cb4234742159f7899e3334027bc1cf30e6
JacobAdhanki <96230023+JacobAdhanki@users.noreply.github.com>
2025-11-13 15:39:49 +0530

    Create user.java

---
+++ akira-backend/src/main/java/com/akira/akira-backend/src/main/java/com/akira/auth/user.java
@@ -0,0 +1,18 @@
+package com.akira.auth;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role; // ADMIN, TEACHER, PARENT
+}
