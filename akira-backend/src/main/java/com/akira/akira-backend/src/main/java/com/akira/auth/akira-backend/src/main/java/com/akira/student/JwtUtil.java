b5c06119fd3ebdc872251426f0c8ded2527a682f
JacobAdhanki <96230023+JacobAdhanki@users.noreply.github.com>
2025-11-13 15:43:29 +0530

    Create JwtUtil.java

---
+++ akira-backend/src/main/java/com/akira/akira-backend/src/main/java/com/akira/auth/akira-backend/src/main/java/com/akira/student/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.akira.auth;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "AKIRA_SECRET_KEY";
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        return !getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
+}
